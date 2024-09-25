a05dff022dc1d6a6b7a652cda74d5aab95525f68
Andrei Nicolae <andreicalutiu@gmail.com>
2024-09-25 15:36:32 +0300

    homepage header and menu added

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+(function() {
+  "use strict";
+
+  const selectTyped = document.querySelector('.typed');
+  if (selectTyped) {
+    let typed_strings = selectTyped.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(',');
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 75,
+      backSpeed: 30,
+      backDelay: 1500
+    });
+  }
+
+  let scrollTop = document.querySelector('.scroll-top');
+
+  function toggleScrollTop() {
+    if (scrollTop) {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+  }
+  scrollTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+
+  window.addEventListener('load', toggleScrollTop);
+  document.addEventListener('scroll', toggleScrollTop);
+  
+})();
