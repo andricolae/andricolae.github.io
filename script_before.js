4064931f7a12ebbeb38eafa0af91b5ee86ecfc99
Andrei Nicolae <andreicalutiu@gmail.com>
2025-05-06 15:25:53 +0300

    small redesign

---
+++ script_before.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#3b82f6',
+                secondary: '#10b981',
+                dark: '#1f2937',
+                light: '#f9fafb'
+            },
+            fontFamily: {
+                poppins: ['Poppins', 'sans-serif']
+            }
+        }
+    }
+}
